af1b70a0d8ee5640e91c2425a2f3e793cdd0fafc
therandommer <T7083588@tees.ac.uk>
2020-02-01 22:19:09 +0000

    Pointer baseline

--- Assets/Scripts/Building/BuildSettings.cs
+++ Assets/Scripts/Building/BuildSettings.cs
@@ -38,7 +38,7 @@ public class BuildSettings : MonoBehaviour
 	[SerializeField]
 	GameObject clearObject; //need a reference to a type of object, so they can be taken when the level is played.
 	[SerializeField]
-	int clearConditionNumber = 0; //this will probably change
+	int clearConditionRequired = 0; //this will probably change
 	[SerializeField]
 	bool isPlayerValid = false; //if the player is placed in a valid location then this will become true;
 	#endregion
@@ -49,6 +49,75 @@ public class BuildSettings : MonoBehaviour
 	List<GameObject> Favourites = new List<GameObject>(); //save the player prefered tiles here
 	#endregion
 
+	#region Setters and Getters
+	public void SetTime(float _newTime)
+	{
+		time = _newTime;
+	}
+	public void IncrementObject(int _type,int _amount)
+	{
+		switch(_type)
+		{
+			case 1:
+				placedTerrain += _amount;
+				break;
+			case 2:
+				placedFriendly += _amount;
+				break;
+			case 3:
+				placedHazards += _amount;
+				break;
+			case 4:
+				placedInteractables += _amount;
+				break;
+			case 5:
+				placedOther += _amount;
+				break;
+			default:
+				break;
+		}
+	}
+	public void SetPlayerSpawn(Vector2 _newSpawn)
+	{
+		if(_newSpawn.x > minBaseLevelLimits.x && _newSpawn.y > minBaseLevelLimits.y &&
+			_newSpawn.x < maxBaseLevelLimits.x && _newSpawn.y < maxBaseLevelLimits.y)
+		{
+			playerSpawnPoint = _newSpawn;
+		}
+		
+	}
+	public void SetLevelLimitX(int _x)
+	{
+		if(_x<minBaseLevelLimits.x)
+		{
+			levelLimits.x = minBaseLevelLimits.x;
+		}
+		if(_x>maxBaseLevelLimits.x)
+		{
+			levelLimits.x = maxBaseLevelLimits.x;
+		}
+		else
+		{
+			levelLimits.x = _x;
+		}
+	}
+	public void SetLevelLimitY(int _y)
+	{
+		if (_y < minBaseLevelLimits.y)
+		{
+			levelLimits.y = minBaseLevelLimits.y;
+		}
+		if (_y > maxBaseLevelLimits.y)
+		{
+			levelLimits.y = maxBaseLevelLimits.y;
+		}
+		else
+		{
+			levelLimits.x = _y;
+		}
+	}
+
+	#endregion
 	void Start()
     {
         
--- Assets/Scripts/Building/UIBaseObject.cs
+++ Assets/Scripts/Building/UIBaseObject.cs
@@ -1,18 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIBaseObject : MonoBehaviour
 {
-    // Start is called before the first frame update
+	int objectID = 0;
+	Sprite mouseObject;
+	Image thisObject;
+	Color startColour;
+	Renderer thisRenderer;
+	bool isMouseHovering = false;
+
     void Start()
     {
-        
+		startColour = gameObject.GetComponent<Renderer>().material.color;
+		thisRenderer = gameObject.GetComponent<Renderer>();
+		thisObject = gameObject.GetComponent<Image>();
     }
 
-    // Update is called once per frame
-    void Update()
+	private void OnMouseEnter()
+	{
+		thisRenderer.material.color = Color.gray;
+		isMouseHovering = true;
+	}
+	private void OnMouseExit()
+	{
+		thisRenderer.material.color = startColour;
+		isMouseHovering = false;
+	}
+
+	void Update()
     {
-        
-    }
+		if (Input.GetMouseButtonUp(0) && isMouseHovering)
+		{
+			//call a function in the pointer
+		}
+	}
 }
