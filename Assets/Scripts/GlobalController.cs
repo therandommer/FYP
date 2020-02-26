e94cdf107bba31b524679b2be9c8fe3aa11db98a
therandommer <T7083588@tees.ac.uk>
2020-02-26 15:44:27 +0000

    Ability to switch between game states now.
    Just need to add a few things to flesh basic gameplay out.

--- Assets/Scripts/GlobalController.cs
+++ Assets/Scripts/GlobalController.cs
@@ -12,6 +12,10 @@ public class GlobalController : MonoBehaviour
 	bool isBuilding = true; //when true building UI enabled, when false playing level.
 	[SerializeField]
 	bool isSaving = false;
+	[SerializeField]
+	bool isGameplay = false;
+	[SerializeField]
+	bool needsUIUpdate = false;
 	#endregion
 	#region UI Elements
 	[Header("UI Elements")]
@@ -20,7 +24,7 @@ public class GlobalController : MonoBehaviour
 	[SerializeField]
 	GameObject GamePlayUI;
 	[SerializeField]
-	GameObject OtherUI;
+	GameObject PauseUI;
 	#endregion
 
 	void Start()
@@ -30,6 +34,75 @@ public class GlobalController : MonoBehaviour
 
     void Update()
     {
-        
+        if(needsUIUpdate)
+		{
+			if(isPaused)
+			{
+				PauseUI.SetActive(true);
+				//enable pause UI as an overlay
+				needsUIUpdate = false;
+			}
+			if(isBuilding)
+			{
+				BuildUI.SetActive(true);
+				GamePlayUI.SetActive(false);
+				//disable other UI elements, enable building canvas
+				needsUIUpdate = false;
+			}
+			if(isGameplay)
+			{
+				GamePlayUI.SetActive(true);
+				BuildUI.SetActive(false);
+				//disable otehr UI elements, enable gameplay canvas
+				needsUIUpdate = false;
+			}
+			if(isSaving)
+			{
+				//save bar overlay
+			}
+			if(!isPaused && PauseUI.activeInHierarchy)
+			{
+				PauseUI.SetActive(false);
+				needsUIUpdate = false;
+			}
+		}
     }
+
+	#region Setters and Getters
+	public void SetIsBuilding(bool _isBuilding)
+	{
+		isBuilding = _isBuilding;
+		needsUIUpdate = true;
+	}
+	public bool GetIsBuilding()
+	{
+		return isBuilding;
+	}
+	public void SetIsGameplay(bool _isGameplay)
+	{
+		isGameplay = _isGameplay;
+		needsUIUpdate = true;
+	}
+	public bool GetIsGameplay()
+	{
+		return isGameplay;
+	}
+	public void SetIsPaused(bool _isPaused)
+	{
+		isPaused = _isPaused;
+		needsUIUpdate = true;
+	}
+	public bool GetIsPaused()
+	{
+		return isPaused;
+	}
+	public void SetIsSaving(bool _isSaving)
+	{
+		isSaving = _isSaving;
+	}
+	public bool GetIsSaving()
+	{
+		return isSaving;
+	}
+	#endregion
 }
